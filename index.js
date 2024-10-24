865190a675d8a3f500a6f49b6ee666ea43673858
unknown <alpucaps@gmail.com>
2024-10-24 21:41:39 +0700

    Fixing Bugs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3016;
 
 // Middleware to parse JSON
 app.use(express.json());
